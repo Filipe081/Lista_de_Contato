055bd311abbe5b2c62b9a8b5ec3c04ac9a6ccc29
Filipe Jose <filipejoserds123@gmail.com>
2024-05-13 06:14:52 -0300

    criando a lista

---
+++ src/styles/variaveis.ts
@@ -0,0 +1,11 @@
+export default {
+  grafite: '#3232CD',
+  vermelho: '#fd4747',
+  vermelhoHover: '#8C1717',
+  cinzaLight: '#ccc',
+  cinza: '#f5f6fa',
+  verde: '#00a86b',
+  verdeHover: '#00cf83',
+  branco: '#ffffff',
+  amarelo: '#ffa555'
+}
